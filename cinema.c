31f0385db94e8c4d6ce6474daad9cd27a8a9d8cf
Maria Clara Souza Muharem <mariacsmuharem@gmail.com>
2026-04-08 01:17:32 -0300

    escada, porta, luzes na escada, parede da escada, caixa de som, etc

--- cinema.c
+++ cinema.c
@@ -3,7 +3,7 @@
 
 #include <GL/glut.h>
 
-// CONTROLE DA CÂMERA
+//controle camera
 float anguloY = 0.0;
 float distancia = 15.0;
 
@@ -11,8 +11,8 @@ int ultimoX;
 int botaoPressionado = 0;
 
 
-// OBJETOS
-// CHÃO
+//objetos da cena
+//chao
 void desenharChao() {
     glColor3f(0.2, 0.2, 0.2);
 
@@ -22,7 +22,7 @@ void desenharChao() {
     glPopMatrix();
 }
 
-// TETO
+//teto
 void desenharTeto() {
     glColor3f(0.15, 0.15, 0.15);
 
@@ -33,25 +33,25 @@ void desenharTeto() {
     glPopMatrix();
 }
 
-// PAREDES
+//parede
 void desenharParedes() {
     glColor3f(0.1, 0.1, 0.1);
 
-    // fundo
+    //fundo
     glPushMatrix();
     glTranslatef(0, 3, -6);
     glScalef(12, 6, 0.2);
     glutSolidCube(1);
     glPopMatrix();
 
-    // esquerda
+    //esq
     glPushMatrix();
     glTranslatef(-6, 3, 0);
     glScalef(0.2, 6, 12);
     glutSolidCube(1);
     glPopMatrix();
 
-    // direita
+    //dir
     glPushMatrix();
     glTranslatef(6, 3, 0);
     glScalef(0.2, 6, 12);
@@ -59,7 +59,7 @@ void desenharParedes() {
     glPopMatrix();
 }
 
-// TELA
+//tela
 void desenharTela() {
     glColor3f(0.9, 0.9, 0.9);
 
@@ -70,43 +70,92 @@ void desenharTela() {
     glPopMatrix();
 }
 
-// CADEIRA
+//cadeira
 void desenharCadeira() {
-    // assento
+    //assento
     glColor3f(0.6, 0.0, 0.0);
     glPushMatrix();
     glScalef(0.8, 0.3, 0.8);
     glutSolidCube(1);
     glPopMatrix();
 
-    // encosto
+    //encosto
     glPushMatrix();
-    glTranslatef(0, 0.5, -0.3);
+    glTranslatef(0, 0.5, 0.3);
     glScalef(0.8, 1.0, 0.2);
     glutSolidCube(1);
     glPopMatrix();
 }
+//escada
+void desenharEscadas() {
+    glColor3f(0.3, 0.3, 0.3);
 
-// FILEIRAS
-void desenharCadeiras() {
+    int linha = 0;
+
     for(int z = -2; z <= 4; z += 2) {
-        for(int x = -4; x <= 4; x += 2) {
-            glPushMatrix();
-            glTranslatef(x, 0.5, z);
-            desenharCadeira();
-            glPopMatrix();
-        }
+        float altura = linha * 0.7;
+
+        glPushMatrix();
+        glTranslatef(0, altura + 0.1, z);
+        glScalef(12, 0.1, 1.8);
+        glutSolidCube(1);
+        glPopMatrix();
+        linha++;
     }
 }
 
-// LUZES
+void desenharParedeEscada() {
+    glColor3f(0.08, 0.08, 0.08);
+    glPushMatrix();
+    glTranslatef(0, 1.15, 5.0); 
+    glScalef(12, 2.0, 0.2); 
+    glutSolidCube(1);
+    glPopMatrix();
+}
+
+void desenharLuzesEscada() {
+    glColor3f(1.0, 0.9, 0.0);
+
+    int linha = 0;
+    for(float z = -2; z <= 4; z += 2) { 
+        float altura = linha * 0.7;
+        float posicoesX[] = {-5.5, -1.0, 1.0, 5.5}; 
+
+        for(int i = 0; i < 4; i++) {
+            glPushMatrix();
+            glTranslatef(posicoesX[i], altura + 0.17, z + 0.9); 
+            glScalef(0.4, 0.02, 0.1); 
+            glutSolidCube(1);
+            glPopMatrix();
+        }
+        linha++;
+    }
+}
+
+//fileira
+void desenharCadeiras() {
+    int linha = 0;
+
+    for(int z = -2; z <= 4; z += 2) {
+        float altura = linha * 0.64;
+        for(int x = -4; x <= 4; x += 2) {
+            glPushMatrix();
+            glTranslatef(x, altura + 0.6, z);
+            desenharCadeira();
+            glPopMatrix();
+        }
+        linha++;
+    }
+}
+
+//luz
 void desenharLuzes() {
     glColor3f(1.0, 1.0, 0.0);
 
     for(int i = -4; i <= 4; i += 2) {
         glPushMatrix();
         glTranslatef(-5.5, 3, i);
-        glutSolidSphere(0.3, 20, 20);
+        glutSolidSphere(0.24, 20, 20);
         glPopMatrix();
 
         glPushMatrix();
@@ -115,26 +164,80 @@ void desenharLuzes() {
         glPopMatrix();
     }
 }
+void desenharCaixasSom() {
+    glColor3f(0.05, 0.05, 0.05);
 
-// DISPLAY
+    //esquerda
+    glPushMatrix();
+    glTranslatef(-4.5, 5, -4);
+    glScalef(0.6, 1.2, 0.6);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    //direita
+    glPushMatrix();
+    glTranslatef(4.5, 5, -4);
+    glScalef(0.6, 1.2, 0.6);
+    glutSolidCube(1);
+    glPopMatrix();
+    glColor3f(0.2, 0.2, 0.2);
+    //esferas
+    glPushMatrix();
+    glTranslatef(-4.5, 5, -3.5);
+    glutSolidSphere(0.2, 20, 20);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(4.5, 5, -3.5);
+    glutSolidSphere(0.2, 20, 20);
+    glPopMatrix();
+}
+
+void desenharPorta() {
+    glColor3f(0.05, 0.05, 0.05);
+    glPushMatrix();
+    glTranslatef(5.9, 1.5, -4.0); 
+    glScalef(0.1, 3.0, 3.5);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    //folha
+    glColor3f(0.2, 0.1, 0.05);
+    glPushMatrix();
+    glTranslatef(5.85, 1.5, -4.0); 
+    glScalef(0.05, 2.9, 3.2);
+    glutSolidCube(1);
+    glPopMatrix();
+    
+    //maçaneta
+    glColor3f(0.8, 0.7, 0.2);
+    glPushMatrix();
+    glTranslatef(5.75, 1.5, -3.0);
+    glutSolidSphere(0.08, 10, 10);
+    glPopMatrix();
+}
+//display
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glLoadIdentity();
-
+    glRotatef(anguloY, 0, 1, 0);
     gluLookAt(0, 5, distancia,
               0, 0, 0,
               0, 1, 0);
-
-    glRotatef(anguloY, 0, 1, 0);
+    
 
     desenharChao();
     desenharTeto();
     desenharParedes();
+    desenharPorta();
     desenharTela();
+    desenharEscadas();
+    desenharLuzesEscada ();
     desenharCadeiras();
+    desenharParedeEscada ();
     desenharLuzes();
-
+    desenharCaixasSom();
     glutSwapBuffers();
     glutPostRedisplay();
 }
